57d460a758925bd313d4c3586e726a9c5c1d79d4
Paul Griffiths <mail@paulgriffiths.net>
2014-06-07 08:47:08 -0400

    Add include path for mysql build.

--- lib/database/mysql/module.mk
+++ lib/database/mysql/module.mk
@@ -3,9 +3,10 @@ local_lib := $(local_dir)/libdatabase_mysql.a
 local_src := $(wildcard $(local_dir)/*.c)
 local_objs := $(subst .c,.o,$(local_src))
 
-libraries += $(local_lib) -libmysqlclient
-libraries += -lmysqlclient -lpthread -lz -lm -lrt -ldl
-LDFLAGS += -L/usr/lib/x86_64-linux-gnu
+libraries += $(local_lib)
+CFLAGS    += -I/usr/include/mysql
+LDFLAGS   += -L/usr/lib/x86_64-linux-gnu
+LDFLAGS   += -lmysqlclient -lpthread -lz -lm -lrt -ldl
 sources   += $(local_src)
 
 $(local_lib): $(local_objs)
